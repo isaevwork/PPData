44e342d432fe9c50ab63a829cbfba19bd0a7baa9
Liam Sav <backgroundpositioncenter@gmail.com>
2024-02-21 03:00:16 +0300

    [parsing-data] - добавил слайд 4.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import warnings
+import pandas as pd
+from pptx.dml.color import RGBColor
 from openpyxl import load_workbook
 from pptx import Presentation
 from datetime import datetime
-import pandas as pd
-from pptx.util import Inches, Pt
-from openpyxl.utils.dataframe import dataframe_to_rows
+from pptx.util import Inches, Pt, Cm
+from pptx.enum.text import PP_PARAGRAPH_ALIGNMENT
+
 
 # Путь к папке work
 user_profile = os.getenv('USERPROFILE')
@@ -19,7 +21,6 @@ folder_name = None
 
 # Глобальная переменная для хранения Excel файла
 wb = None
-
 
 # Расширения для изображений
 extensions = ['.jpg', '.jpeg', '.png']
@@ -38,7 +39,7 @@ if excel_files:
     excel_file_path = excel_files[0]
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        wb = load_workbook(filename=excel_file_path)
+        wb = load_workbook(filename=excel_file_path, data_only=True)
         print(f"Excel файл {os.path.basename(excel_file_path)} найден в папке {os.path.dirname(excel_file_path)}.")
 
         # Извлекаем имя папки из пути
@@ -46,36 +47,13 @@ if excel_files:
 else:
     print("Файл Excel не найден в папке work.")
 
-
-
 # Загружаем презентацию
 prs = Presentation(os.path.join(os.getenv('USERPROFILE'), 'Downloads', 'parser', 'PPData', 'FDTemp.pptx'))
-
-
-
-# slide_layout = root.slide_layouts[1]
-# slide = root.slides.add_slide(slide_layout)
-#
-# for row in ws.iter_rows(min_row=1):
-#     title = row[0].value
-    # content = row[1].value
-
-    # title_placeholder = slide.shapes.title
-    # content_placeholder = slide.placeholders[1]
-
-    # title_placeholder.text = str(title)
-    # content_placeholder.text = str(content)
-
-# title_placeholder = slide.shapes.title
-# content_placeholder = slide.placeholders[1]
-
-# title_placeholder.text = str("A" + str(len(title_placeholder.text)))
-# content_placeholder.text = str(content)
 
 # Слайд № 1
 # Задаем имя пациента, врача и дату
 left = Inches(2.9)  # Расстояние от правого края слайда
-top = Inches(7.75)   # Расстояние от верхнего края слайда
+top = Inches(7.75)  # Расстояние от верхнего края слайда
 width = Inches(4)
 height = Inches(0.5)  # Высота, чтобы занять всю высоту слайда
 name_textbox = prs.slides[0].shapes.add_textbox(left, top, width, height)
@@ -87,7 +65,7 @@ p.font.size = Pt(14)
 p.font.bold = False
 # -------------------------------------------------
 date_left = Inches(5)
-date_top = Inches(8.9)   # Расстояние от верхнего края слайда
+date_top = Inches(8.9)  # Расстояние от верхнего края слайда
 date_width = Inches(4)
 name_textbox = prs.slides[0].shapes.add_textbox(date_left, date_top, date_width, height)
 tf_date = name_textbox.text_frame
@@ -99,7 +77,7 @@ print("Первый слайд готов")
 # Слайд № 2
 # Задаем имя пациента вдоль правой границы
 left = Inches(6.9)  # Расстояние от правого края слайда
-top = Inches(7.9)   # Расстояние от верхнего края слайда
+top = Inches(7.9)  # Расстояние от верхнего края слайда
 width = Inches(3)
 height = Inches(2)  # Высота, чтобы занять всю высоту слайда
 name_textbox = prs.slides[1].shapes.add_textbox(left, top, width, height)
@@ -115,7 +93,7 @@ print("Второй слайд готов")
 # Слайд № 3
 # Вставляем изображения и ФИО пациента
 left = Inches(6.9)  # Расстояние от правого края слайда
-top = Inches(7.9)   # Расстояние от верхнего края слайда
+top = Inches(7.9)  # Расстояние от верхнего края слайда
 width = Inches(3)
 height = Inches(2)  # Высота, чтобы занять всю высоту слайда
 name_textbox = prs.slides[2].shapes.add_textbox(left, top, width, height)
@@ -129,7 +107,7 @@ p.font.size = Pt(14)
 
 # Массив имен изображений с префиксом папки
 # TODO image_names = [f"{folder_name}_{image}" for image in ["2.1", "2.2", "2.3", "2.4"]]
-image_names = ["2.1", "2.2", "2.3", "2.4",]
+image_names = ["2.1", "2.2", "2.3", "2.4", ]
 
 # Путь к моим изображениям
 image_folder = os.path.join(work_folder, folder_name)
@@ -169,7 +147,7 @@ print("Третий слайд готов")
 # Слайд № 4
 # Вставляем таблицу из Excel файла и ФИО пациента
 left = Inches(6.9)
-top = Inches(7.9)   # Расстояние от верхнего края слайда
+top = Inches(7.9)  # Расстояние от верхнего края слайда
 width = Inches(3)
 height = Inches(2)
 name_textbox = prs.slides[3].shapes.add_textbox(left, top, width, height)
@@ -181,43 +159,109 @@ p.text = f"{folder_name}"
 p.font.bold = False
 p.font.size = Pt(14)
 
-#
-# # Создаем пустой DataFrame
-# fourSlide_data = []
-# ws = wb["Лист2"]
-#
-# # Проходимся по строкам и столбцам в Excel и добавляем их в DataFrame
-# for row in ws.iter_rows(values_only=True):
-#     fourSlide_data.append(row)
-#
-#
-# # Создаем DataFrame из данных
-# df = pd.DataFrame(fourSlide_data).iloc[1:9, 14:18]
-# print(df.shape)
-#
-# # Получаем количество строк и столбцов в таблице
-# num_rows, num_cols = df.shape
-#
-# # Определяем размеры и позицию таблицы на слайде
-# left = Inches(0.55)
-# top = Inches(6.2)
-# width = Inches(7.1)
-# height = Inches(3.5)
-#
-# # Добавляем таблицу на слайд
-# table = prs.slides[3].shapes.add_table(num_rows, num_cols, left, top, width, height).table
-#
-# # Заполнение таблицы данными из DataFrame
-# # Заполнение таблицы данными из Excel
-# for r in range(num_rows):
-#     for c in range(num_cols):
-#         table.cell(r, c).text = str(df.iloc[r, c])
-#
+ws = wb["Лист2"]
+
+# Создаем пустой DataFrame
+slideThree_data = []
+
+# Проходимся по строкам и столбцам в Excel и добавляем их в DataFrame
+for row in ws.iter_rows(values_only=True):
+    slideThree_data.append(row)
+
+# Создаем DataFrame из данных
+df = pd.DataFrame(slideThree_data).iloc[0:9, 14:18]
+sub_up_df = pd.DataFrame(slideThree_data).iloc[1:2, 1:13]
+sub_lower_df = pd.DataFrame(slideThree_data).iloc[2:3, 1:13]
+
+
+# Получаем количество строк и столбцов в таблице
+num_rows, num_cols = df.shape
+sub_up_num_rows, sub_up_num_cols = sub_up_df.shape
+sub_lower_num_rows, sub_lower_num_cols = sub_lower_df.shape
+
+# Определяем размеры и позицию таблицы на слайде
+left = Inches(0.55)
+top = Inches(6.2)
+width = Inches(7.1)
+height = Inches(3.5)
+
+sub_up_left = Inches(1.05)
+sub_up_top = Inches(2.65)
+sub_up_width = Inches(6.25)
+sub_up_height = Inches(0.4)
+
+sub_lower_left = Inches(1.05)
+sub_lower_top = Inches(3.67)
+sub_lower_width = Inches(6.2)
+sub_lower_height = Inches(0.4)
+
+# Добавляем таблицу на слайд
+table = prs.slides[3].shapes.add_table(9, 3, left, top, width, height).table
+sub_up_table = prs.slides[3].shapes.add_table(1, 12, sub_up_left, sub_up_top, sub_up_width, sub_up_height).table
+sub_lower_table = prs.slides[3].shapes.add_table(1, 12, sub_lower_left, sub_lower_top, sub_lower_width, sub_lower_height).table
+
+# Функция для форматирования значения
+def format_value(value):
+    if isinstance(value, (float, int)):
+        if (r == 7 and c == 1) or (r == 7 and c == 2) or (r == 8 and c == 1):
+            # Форматирование процентного значения
+            return "{:.2f}%".format(value * 100)
+        else:
+            # Округление числовых значений до сотых
+            return str(round(value, 2))
+    else:
+        return str(value) if value is not None else ""
+
+
+# Заполнение таблицы данными из DataFrame и центрирование текста
+for r in range(num_rows):
+    for c in range(num_cols):
+        value = df.iloc[r, c]
+        cell = table.cell(r, c)
+        cell.text = format_value(value)
+        if c == 1 or c == 2:
+            cell.text_frame.paragraphs[0].alignment = PP_PARAGRAPH_ALIGNMENT.CENTER
+
+for r in range(sub_up_num_rows):
+    for c in range(sub_up_num_cols):
+        value = sub_up_df.iloc[r, c]
+        cell = sub_up_table.cell(r, c)
+        cell.text = str(value)
+
+for r in range(sub_lower_num_rows):
+    for c in range(sub_lower_num_cols):
+        value = sub_lower_df.iloc[r, c]
+        cell = sub_lower_table.cell(r, c)
+        cell.text = str(value)
+        cell.text_frame.paragraphs[0].font.size = Pt(10)
+
+sub_lower_table.columns[0].width = Inches(0.8)
+sub_lower_table.columns[4].width = Inches(0.4)
+sub_lower_table.columns[5].width = Inches(0.4)
+sub_lower_table.columns[6].width = Inches(0.4)
+sub_lower_table.columns[7].width = Inches(0.4)
+sub_lower_table.columns[11].width = Inches(0.8)
+
+# Устанавливаем текст "Индекс Тона" в ячейку
+table.cell(4, 0).text = "Индекс Тона"
+# Объединяем ячейки 4 и 5 в первом столбце
+table.cell(4, 0).merge(table.cell(5, 0))
 
 
 
+# Устанавливаем прозрачный цвет заливки для каждой ячейки таблицы
+for row in sub_up_table.rows:
+    for cell in row.cells:
+        cell.fill.solid()
+        cell.text_frame.paragraphs[0].font.color.rgb = RGBColor(0, 0, 0)
+        cell.fill.fore_color.rgb = RGBColor(255, 255, 255)
+
+for row in sub_lower_table.rows:
+    for cell in row.cells:
+        cell.fill.solid()
+        cell.text_frame.paragraphs[0].font.color.rgb = RGBColor(0, 0, 0)
+        cell.fill.fore_color.rgb = RGBColor(255, 255, 255)
 
 
 if folder_name:
     prs.save(os.path.join(work_folder, f"{folder_name}.pptx"))
-
